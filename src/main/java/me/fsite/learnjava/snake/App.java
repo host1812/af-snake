355600c820ce563482cb82d099cdc49a59f4fcb1
host1812 <host1812@yandex.ru>
2016-05-22 10:00:21 -0700

    Initial commit.

---
+++ src/main/java/me/fsite/learnjava/snake/App.java
@@ -0,0 +1,13 @@
+package me.fsite.learnjava.snake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
